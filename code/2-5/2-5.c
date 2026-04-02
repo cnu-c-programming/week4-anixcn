7deb9a14485cd1f933d674b88d383003e9718078
anixcn <anixcnu@gmail.com>
2026-04-02 23:25:19 +0900

    Update 2-5.c

--- code/2-5/2-5.c
+++ code/2-5/2-5.c
@@ -2,13 +2,13 @@
 #include <math.h>
 
 int main() {
-	double x = 9.0;
-	double y = 2.5;
+    double x = 9.0;
+    double y = 2.5;
 
-	printf("%.2f\n", sqrt(x));
-	printf("%.2f\n", pow(x, y));
-	printf("%.2f\n", ceil(y));
-	printf("%.2f\n", floor(y));
+    printf("%.2f\n", sqrt(x));  
+    printf("%.2f\n", pow(x, y)); 
+    printf("%.1f\n", ceil(y));  
+    printf("%.1f\n", floor(y));  
 
-	return 0;
+    return 0;
 }
