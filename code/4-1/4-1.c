8dd6be2e295c43ceb728f04627e7ee345f8e4733
anixcn <anixcnu@gmail.com>
2026-04-02 23:50:38 +0900

    Update 4-1.c

--- code/4-1/4-1.c
+++ code/4-1/4-1.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    int a[5] = { 1,2,3,4,5 };
-
-    for (int i = 0; i < 5; i++) {
+    int a[5] = {1, 2, 3, 4, 5};
+    
+    for (int i=0; i<5; i++)
         printf("%d ", a[i]);
-    }
     printf("\n");
 
-    int b[5] = { 1,2 };
-
-    for (int i = 0; i < 5; i++) {
+    int b[5] = {1, 2};
+    
+    for (int i=0; i<5; i++)
         printf("%d ", b[i]);
-        printf("\n");
-    }
+    printf("\n");
 
-        int c[5] ={ 0 };
+    int c[5] = {0};
+    for (int i=0; i<5; i++)
+        printf("%d ", c[i]);
+    printf("\n");
 
-        for (int i = 0; i < 5; i++) {
-            printf("%d ", c[i]);
-            printf("\n");
-        }
+    int d[] = {10, 20, 30};
+    for (int i=0; i<(sizeof(d) / sizeof(int)); i++)
+        printf("%d ", d[i]);
+    printf("\n");
 
-        int d[] = { 10,20,30 };
-
-        for (int i = 0; i < (sizeof(d) / sizeof(int)); i++) {
-            printf("%d ", d[i]);
-            printf("\n");
-        }
     return 0;
 }
