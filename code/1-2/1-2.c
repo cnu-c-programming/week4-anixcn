face7d304663d8064e66560d3ae99ba57343df8a
anixcn <anixcnu@gmail.com>
2026-04-02 23:00:58 +0900

    Update 1-2.c

--- code/1-2/1-2.c
+++ code/1-2/1-2.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
+#include "util.h"
 
 int main() {
-	void print();
-	int square(int n);
-
-	int num = 5;
-	int result = square(num);
-	printf("%d %d\n", num, result);
-
-	return 0;
+    for(int i=0; i<100; i++) {
+        if (i%2 == 0)
+            logger("even");
+        else
+            logger("odd");
+    }
+    return 0;
 }
-
-void print() {
-	printf("Welcome to Computer Programmig 03 Lab!\n");
-}
-
-int square(int n) {
-	return n * n;
-}
-
-
-
