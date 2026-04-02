49a820d80e7aecd17fbd93f070545fde101f5e12
anixcn <anixcnu@gmail.com>
2026-04-02 23:10:29 +0900

    Update 2-3.c

--- code/2-3/2-3.c
+++ code/2-3/2-3.c
@@ -2,20 +2,20 @@
 #include <string.h>
 
 int main() {
-	char a[64] = "Hello";
-	char b[64] = "Hello";
-	char c[128];
+    char a[64] = "Hello";
+    char b[64] = "Hello";
+    char c[128];
 
-	size_t len(const char* a)
-	printf("length of a: %d\n", len);
+    int len = strlen(a); 
+    printf("length of a: %d\n", len);
 
-	char* strcpy(char* a, const char* c)
-	printf("a: %s, c: %s\n", a, c);
+    strcpy(c, a); 
+    printf("a: %s, c: %s\n", a, c);
 
-	printf("c: %s\n", c);
+    printf("c: %s\n", c);
 
-	printf("strcmp(%s, %s) = %d\n", a, a, int strcmp(const char* a, const char* a));
-	printf("strcmp(%s, %s) = %d\n", a, c, int strcmp(const char* a, const char* c));
-	
-	return 0;
+    printf("strcmp(%s, %s) = %d\n", a, a, strcmp(a, a));
+    printf("strcmp(%s, %s) = %d\n", a, c, strcmp(a, c));
+    
+    return 0;
 }
